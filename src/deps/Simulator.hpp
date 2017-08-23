12252875b8b50139655257086b7d2585c9887020
Ryan Wunderly <rywunder@umich.edu>
2017-08-23 19:09:47 -0400

    Create Game Logic

--- src/deps/Simulator.hpp
+++ src/deps/Simulator.hpp
@@ -43,9 +43,6 @@ public:
 	// Creates and intializes the vehicle on the environment
 	void createVehicle(Program* prog);
 
-	//Returns true if payer won game or false if player lost the game
-	bool getGameResults();
-
 	// Returns the vector of Roombas
 	const std::vector<Roomba>& getRoombaList();
 
@@ -82,6 +79,9 @@ private:
 	// Return 0: not in goal, 1: in goal, 2: in incorrect goal
 	int roombaInGoal(Roomba&);
 
+	// Return 0: not in goal, 1: in goal, 2: in incorrect goal
+	int roombaInGoal(Roomba&);
+
 	// Function keeps roombas stationary
 	std::function <void(Roomba&)> updateRoombaLocation;
 
@@ -98,7 +98,6 @@ private:
 
 	int sizeEnvironment = 10; // Default 10X10m
 	int score = 0;
-	bool gameResults = false;
 
 	std::queue<std::function <void(Vehicle&)> > actionQueue;
 
