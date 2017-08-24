b702fd076ec2af9ff2312bb094f2977291bffd49
dziedada <dziedada@umich.edu>
2017-08-23 23:00:45 -0400

    Make a Constants namespace.

---
+++ src/deps/Constants.hpp
@@ -0,0 +1,10 @@
+namespace Constants
+{
+	constexpr float roombaMass = 5;
+	constexpr float vehicleMass = 50;
+	constexpr float obstacleMass = 15;
+	constexpr float defaultSpeed = 0.01;
+	constexpr float arenaSizeX = 10;
+	constexpr float arenaSizeY = 10;
+	constexpr float pi = 3.14159265358979f;
+}
--- src/deps/Roomba.cpp
+++ src/deps/Roomba.cpp
@@ -1,7 +1,12 @@
 #include "Roomba.hpp"
+#include "Constants.hpp"
 
 Roomba::Roomba(float xInit, float yInit, float angleInit, float radiusInit,
-	Program *program, float *color) : Entity(xInit,yInit,yawInit,radiusInit,program),
+	Program *program, float *color) : AnimatedEntity() : Entity(xInit,yInit,yawInit,radiusInit,program),
 	color {color[0],color[1],color[2]}, roombaBody {Circle(xInit,yInit,angleInit,radiusInit,
 	program,[]{1.0f,1.0f,1.0f})}, panelLarge {Rectangle(xInit,yInit,angleInit,radiusInit,
-	program,color,radius * 0.7f,radius * 0.3f)}, panelSmall {Rectangle(xInit,yInit,)}
+	program,color,radiusInit * 0.7f,radiusInit * 0.3f)}, panelSmall {Rectangle(xInit,yInit,
+	angleInit,radiusInit,program,color,radiusInit * 0.3f,radiusInit * 0.14f)}
+{
+
+}
