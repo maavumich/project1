f08244c9c003a27b2023ca82c358d957035a9486
dziedada <dziedada@umich.edu>
2017-08-29 17:40:32 -0400

    Take revisions and revise.

--- src/deps/Circle.hpp
+++ src/deps/Circle.hpp
@@ -8,14 +8,13 @@ class Circle : public Entity
 public:
 	Circle(float xInit, float yInit, float angleInit, float radiusInit,
 		const float colorIn[], Program *program);
-	~Circle() noexcept;
 	virtual void update();
 	virtual void render();
 private:
 	float color[3];
 	// Data holding the opengl object ids needed to render
-	GLuint VBO;
-	GLuint VAO;
+	unsigned int VBO;
+	unsigned int VAO;
 	// Holds the vertices to be rendered as well as the color to use
 	// Will contain 360 triangles to be rendered to make a circle
 	float renderData[360 * 9 * 2];
